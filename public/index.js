9b30b28c15abe2f14523771f3d1121895d5ec71b
fiona <tseringshrestha>
2014-04-14 02:19:47 -0400

    Accepting auto-corrections

--- public/index.js
+++ public/index.js
@@ -1,10 +1,11 @@
 (function () {
-    $(function (){
-        var $content = $("<div></div>").appendTo("body")
+    'use strict';
+    $(function () {
+        var $content = $('<div></div>').appendTo('body');
         if (Math.random() < 0.5) {
-            $content.text("head")
+            $content.text('head');
         } else {
-            $content.text("tail")
+            $content.text('tail');
         }
     });
-})();
+}());
